43bee0a02b51507993c0c138d8157b183f3ad874
Olivier Tabone <olivier.tabone@ripplemotion.fr>
2012-06-07 20:02:40 +0200

    [F] cut the crap

--- setup.py
+++ setup.py
@@ -1,89 +1,32 @@
-# import ez_setup
-# ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+   name            = "RestMQ",
+   version         = "1.0",
 
-import sys, os, os.path, subprocess
-# print sys.argv
-# try:
-#     from twisted import plugin
-# except ImportError, e:
-#     print >>sys.stderr, "setup.py requires Twisted to create a proper RestMQ installation."
-#     print >>sys.stderr, "Please install it before continuing."
-#     sys.exit(1)
+   packages        = find_packages('src') + ['twisted.plugins'],
+   package_dir		= {
+      ''			: 'src',
+   },
+   include_package_data = True,
 
-# from setuptools.command import easy_install
-# import pkg_resources as pkgrsrc
+   zip_safe = False,
 
-from distutils import log
-log.set_threshold(log.INFO)
-
-# disables creation of .DS_Store files inside tarballs on Mac OS X
-os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
-os.environ['COPYFILE_DISABLE'] = 'true'
-
-postgenerate_cache_commands = ('build', 'build_py', 'build_ext',
-    'build_clib', 'build_scripts', 'install', 'install_lib',
-    'install_headers', 'install_scripts', 'install_data',
-    'develop', 'easy_install')
-
-pregenerate_cache_commands = ('sdist', 'bdist', 'bdist_dumb',
-    'bdist_rpm', 'bdist_wininst', 'upload', 'bdist_egg', 'test')
-
-def autosetup():
-    from setuptools import setup, find_packages
-    return setup(
-        name            = "RestMQ",
-        version         = "1.0",
-
-        packages        = find_packages('src') + ['twisted.plugins'],
-		package_dir		= {
-			''			: 'src',
-		},
-        include_package_data = True,
-
-        zip_safe = False,
-
-        entry_points    = {
-            'setuptools.file_finders'   : [
-                'git = restmq.setup_extras:find_files_for_git',
-            ],
-        },
+   entry_points    = {
+      'setuptools.file_finders'   : [
+         'git = restmq.setup_extras:find_files_for_git',
+         ],
+      },
         
-        install_requires = [
-            'zope.interface',
-            'twisted',
-            'simplejson',
-            'cyclone',
-        ],
+   install_requires = [
+      'twisted',
+      'simplejson',
+      'cyclone',
+    ],
         # metadata for upload to PyPI
-        author          = "Gleicon Moraes",
-        author_email    = "gleicon@gmail.com",
-        description     = "REST/JSON/HTTP based message queue",
-        keywords        = "rest json message queue",
-        url             = "https://github.com/gleicon/restmq",
-    )
+   author          = "Gleicon Moraes",
+   author_email    = "gleicon@gmail.com",
+   description     = "REST/JSON/HTTP based message queue",
+   keywords        = "rest json message queue",
+   url             = "https://github.com/gleicon/restmq",
+)
 
-
-if(__name__ == '__main__'):
-    skip_cache_regeneration = 'egg_info' in sys.argv
-    if not skip_cache_regeneration:
-        try:
-            from twisted import plugin
-        except ImportError, e:
-            print >>sys.stderr, "setup.py requires Twisted to create a proper RestMQ installation."
-            print >>sys.stderr, "Please install it before continuing."
-            sys.exit(1)
-    if(not skip_cache_regeneration and sys.argv[-1] in pregenerate_cache_commands):
-       
-        dist_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src')
-        if(dist_dir not in sys.path):
-            sys.path.insert(0, dist_dir)
-
-        from restmq import setup_extras
-        print 'Regenerating plugin cache ...'
-        setup_extras.regeneratePluginCache()
-
-    dist = autosetup()
-    if(not skip_cache_regeneration and sys.argv[-1] in postgenerate_cache_commands):
-        subprocess.Popen(
-            [sys.executable, '-c', 'from restmq import setup_extras; setup_extras.regeneratePluginCache(); print "Regenerating plugin cache..."'],
-        ).wait()
